51825e663f5cb12b5ec637bf5df1497c16932e92
uniquefairty <15991160436@163.com>
2019-08-26 19:13:51 +0800

    练习题检查

--- C_check/C_check/main.c
+++ C_check/C_check/main.c
@@ -1,7 +1,84 @@
 #include<stdio.h>
+#include<string.h>
 #include<Windows.h>
+#pragma warning(disable:4996)
 
+int main()
+{
+	unsigned char a = 0xa5;
+	unsigned char b =(~a) >>4 + 1;
+	printf("%d\n", b);
 
+	system("pause");
+	return 0;
+}
+int main15()
+{
+	unsigned short sht = 0;
+	sht--;
+	printf("%d", sht);
+	system("pause");
+	return 0;
+}
+int main15_3()
+{
+	int x = 1, y = 012;
+	printf("%d", y*x++);
+	system("pause");
+	return 0;
+}
+int main15_1_()
+{
+	int j = 4;
+	int i = j;
+	for (; i <= 2 * j; i++)
+	{
+		switch (i / j)
+		{
+		case 0:
+		case 1:
+			printf("*");
+			break;
+		case 2:
+			printf("#");
+		}
+	}
+	system("pause");
+	return 0;
+}
+int main15_1()
+{
+	unsigned int a = 0xFFFFFFF7;
+	unsigned char i = (unsigned char)a;
+	char *b = (char *)&a;//地址类型发生改变，使a的地址完全存入b中，但没有改变里面的内容
+
+	printf("%08x,%08x", i, *b);//000000f7 fffffff7
+	system("pause");
+	return 0;
+}
+int main15_2()
+{
+	char str[] = "ABCDEFG";
+	char *p1, *p2;
+	p1 = "abcd"; p2 = "efgh";
+
+	strcpy(str + 1, p2 + 1);
+	strcpy(str + 3, p1 + 3);
+	printf("%s\n", str);
+	system("pause");
+	return 0;
+}
+int main14_3()
+{
+	int arr[] = { 6, 7, 8, 9, 10 };
+	int *ptr = arr;
+	*ptr++ += 123;
+	printf("%d,%d\n", *ptr, *++ptr);
+
+	printf("%d", arr[0]);
+	system("pause");
+	return 0;
+}
 int f(int x, int y)
 {
 	return  (x&y) + ((x^y) >> 1);
@@ -15,16 +92,16 @@ int fun(char *s)
 	}
 	return(p - s);
 }
-int main()
+int main1()
 {
 	/*int result = f(2, 4);
 	printf("%d\n", result);*/
 
-	/*unsigned char a = 0xa5;
+	unsigned char a = 0xa5;
 	unsigned char b = ~a >> 4 + 1;
-	printf("%d\n", b);*/
+	printf("%d\n", b);
 
-	printf("%d\n",fun("goodbye!"));
+	//printf("%d\n",fun("goodbye!"));
 
 	system("pause");
 	return 0;
