9eb15d281ca723b63b906b81dc4c35ea145dbec5
uniquefairty <15991160436@163.com>
2019-09-29 10:21:28 +0800

    模板初阶

--- Model1/Model1/main.cpp
+++ Model1/Model1/main.cpp
@@ -50,7 +50,6 @@ int main()
 	Add('1', '2');
 	//模板参数一般不进行类型转化
 	Add(1,(int) 1.0);//显式实例化
-
 	Add<int>(1, 2.0);//编译器：可能进行隐式类型转化
 	return 0;
 }
@@ -124,3 +123,134 @@ int main()
 }
 #endif
 
+#include<assert.h>
+//typedef int DateType;
+
+//动态类型顺序表
+template<class T>
+class SeqList
+{
+public:
+	SeqList(size_t capacity=10)
+		:_array(new T[capacity])
+		, _size(0)
+		, _capacity(capacity)
+	{}
+	~SeqList()
+	{
+		if (_array)
+		{
+			delete[] _array;
+			_array = nullptr;
+			_capacity = 0;
+			_size = 0;
+		}
+	}
+	void PushBack(const T& data);//尾插
+	void PopBack()
+	{
+		_size--;
+	}
+	//任意位置插入和删除
+	void Insert(size_t pos, const T& data)
+	{
+		_CheckCapacity();
+		for (size_t i = _size-1; i >=pos; i--)
+		{
+			_array[i + 1] = _array[i];
+		}
+		_array[pos] = data;
+		_size++;
+	}
+	void Erase(size_t pos)
+	{
+		for (size_t i = pos; i < _size; i++)
+		{
+			_array[i] = _array[i + 1];
+		}
+		_size--;
+	}
+
+	//在const类型成员函数中不能修改成员变量
+	//修饰this指针
+	size_t size() const
+	{
+		return _size;
+	}
+	size_t Capacity()const
+	{
+		return _capacity;
+	}
+	bool Empty()const
+	{
+		return 0 == _size;
+	}
+
+	T& operator[](size_t index)
+	{
+		assert(index < _size);
+		return _array[index];
+	}
+
+	const T&operator[](size_t index)const
+	{
+		assert(index < _size);
+		return _array[index];
+	}
+
+private:
+	void _CheckCapacity()
+	{
+		if (_size == _capacity)
+		{
+			//开辟新空间
+			T* array = new T[2 * _capacity];
+			//拷贝元素
+			//memcpy(array, _array, _size*sizeof(T));
+			for (size_t i = 0; i < _size; ++i)
+			{
+				array[i] = _array[i];
+			}
+			//释放旧空间
+			delete[] _array;
+			_array = array;
+			_capacity *= 2;
+		}
+	}
+private:
+	T* _array;
+	size_t _size;
+	size_t _capacity;
+};
+
+//注意：类模板中的函数放在类外进行定义时，需要加模板参数列表
+template<class T>
+void SeqList<T>::PushBack(const T& data)
+{
+	_CheckCapacity();
+	_array[_size++] = data;
+}
+
+void TestSeqList()
+{
+	SeqList<int> s1;
+	s1.PushBack(1);
+	s1.PushBack(2);
+	s1.PushBack(3);
+	s1.PushBack(4);
+	cout << s1.size() << endl;
+	cout << s1.Capacity() << endl;
+	s1.Insert(2, 8);
+	cout << s1[2] << endl;
+	s1[2] = 10;
+
+	const SeqList<double> s2;
+	//s2[0];
+
+}
+
+int main()
+{
+	TestSeqList();
+	return 0;
+}
