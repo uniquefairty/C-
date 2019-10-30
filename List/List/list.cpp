2d2d733c6eef38da5006a515d343603a461cb0f5
uniquefairty <15991160436@163.com>
2019-10-30 08:00:45 +0800

    list的模拟实现

--- List/List/list.cpp
+++ List/List/list.cpp
@@ -167,11 +167,326 @@ namespace bit
 		T _data;
 	};
 	//List迭代器：将节点类型的指针重新封装
+	//封装正向迭代器：
 	template<class T>
 	struct list_iterator
 	{
 		typedef ListNode<T> Node;
 		typedef list_iterator<T> Self;
+	public:
+		list_iterator(Node* pCur)
+			:_pCur(pCur)
+		{}
+		//按指针的方式进行应用
+		T& operator*()
+		{
+			return _pCur->_data;
+		}
+		T* operator->()
+		{
+			return &(_pCur->data);
+		}
+		//3.移动
+		Self& operator++()
+		{
+			_pCur = _pCur->_pNext;
+			return *this;
+		}
+		Self operator++(int)
+		{
+			Self temp(*this);
+			_pCur = _pCur->_pNext;
+			return temp;
+		}
+		Self& operator--()
+		{
+			_pCur = _pcur->_pPer;
+			return *this;
+		}
+		Self operator--(int)
+		{
+			Self temp(*this);
+			_pCur = _pCur->__pPer;
+			return temp;
+		}
+		//4.比较
+		bool operator!=(const Self& s)
+		{
+			return _pCur != s._pCur;
+		}
+		bool operator==(const Self& s)
+		{
+			return _pCur == s._pCur;
+		}
+		Node* _pCur;
 	};
 
+	//封装反向迭代器
+	template<class Iterator,class T>
+	struct list_reverse_iterator
+	{
+		typedef list_reverse_iterator<Iterator, T> Self;
+	public:
+		list_reverse_iterator(Iterator it)
+		:_it(it)
+		{}
+		//按指针的方式进行应用
+		T& operator*()
+		{
+			Iterator temp = _it;
+			--temp;
+			return *temp;
+		}
+		T* operator->()
+		{
+			return &(operator*());
+		}
+		//3.移动
+		Self& operator++()
+		{
+			--it;
+			return *this;
+		}
+		Self operator++(int)
+		{
+			Self temp(*this);
+			_it--;
+			reurn temp;
+		}
+		Self& operator--()
+		{
+			++it;
+			return(*this);
+		}
+		Self operator--(int)
+		{
+			Self temp(*this);
+			++it;
+			return temp;
+		}
+		//4.比较
+		bool operator!=(const Self& s)
+		{
+			return _it != s._it;
+		}
+		bool operator==(const Self& s)
+		{
+			return _it == s._it;
+		}
+	
+		Iterator _it;
+	};
+
+	template<class T>
+	class list
+	{
+		typedef ListNode<T> Node;
+	public:
+		typedef list_iterator<T> iterator;
+		typedef list_reverse_iterator<iterator, T> reverse_iterator;
+		//list的构造
+		list()
+		{
+			CreateHead();
+		}
+		list(int n, const T& data)
+		{
+			CreateHead();
+			for (size_t i = 0; i < n; i++)
+				push_back(data);
+		}
+		template<class Iterator>
+		list(Iterator first, Iterator last)
+		{
+			CreateHead();
+			while (first != last)
+			{
+				push_back(*first);
+				++first;
+			}
+		}
+		list(const list<T>& L)
+		{
+			CreateHead();
+			Node *pCur = L._pHead->_pNext;
+			while (pCur != L._pHead)
+			{
+				push_back(pCur->data);
+				pCur = pCur->_pNext;
+			}
+		}
+		//list(const list<T>& L)
+		//{
+		//	CreateHead();
+		//	//用L中的元素构造临时的temp，然后与当前对象交换
+		//	list<T> temp(L.cbegin(), L.cend());
+		//	this->swap(temp);
+		//}
+
+		list<T>& operator=(const list<T>& L)
+		{
+			if (this != &L)
+			{
+				clear();
+				Node* pCur = L._pHead->_pNext;
+				while (pCur != _pHead)
+				{
+					push_back(pCur->_data);
+					pCur = pCur->_pNext;
+				}
+			}
+			return *this;
+		}
+		list<T>& operator=(const list<T> L)
+		{
+			this->swap(L);
+			return *this;
+		}
+		~list()
+		{
+			clear();
+			delete _pHead;
+		}
+		/////////////////////////////////////
+		//迭代器
+		iterator begin()
+		{
+			return iterator(_pHead->_pNext);
+		}
+		iterator end()
+		{
+			return iterator(_pHead);
+		}
+		reverse_iterator rbegin()
+		{
+			return end();
+		}
+		reverse_iterator rend()
+		{
+			return begin();
+		}
+		/////////////////////////////////////
+		//容量
+		size_t size()const
+		{
+			size_t count = 0;
+			Node* pCur = _pHead->_pNext;
+			while (pCur != _pHead)
+			{
+				++count;
+				pCur = pCur->_pNext;
+			}
+			return count;
+		}
+		size_t empty() const
+		{
+			return _pHead->_pNext == _pHead;
+		}
+		void resize(size_t newsize, const T&data = T())
+		{
+			size_t oldsize = size();
+			if (newsize > oldsize)
+			{
+				//结点增多
+				for (size_t i = oldsize; i < newsize; ++i)
+					push_back(data);
+			}
+			else
+			{
+				//结点减少
+				for (size_t i = newsize; i < oldsize; ++i)
+					pop_back();
+			}
+		}
+		/////////////////////////////////////
+		//元素访问
+		T& front()
+		{
+			return _pHead->_pNext->data;
+		}
+		const T& front()const
+		{
+			return _pHead->_pNext->data;
+		}
+		T& back()
+		{
+			return _pHead->_pPer->_data;
+		}
+		const T& back()const
+		{
+			return _pHead->_pPPer->_data;
+		}
+		///////////////////////////////////
+		//修改
+		void push_back(const T& data)
+		{
+			insert(end(), data);
+		}
+		void pop_back()
+		{
+			erase(--end());
+		}
+		void push_front(const T& data)
+		{
+			insert(begin(), data);
+		}
+		void pop_front()
+		{
+			erase(begin());
+		}
+		//在pos为之前插入值为data的结点
+		iterator insert(iterator pos, const T& data)
+		{
+			Node* pNewNode = new Node(data);
+			Node* pCur = pos._pCur;
+			pNewNode->_pPre = pCur->_pPre;
+			pNewNode->_pNext = pCur;
+			pNewNode->_pPre->_pNext = pNewNode;
+			pCur->_pPre = pNewNode;
+
+			return iterator(pNewNode);
+		}
+		//删除pos位置的结点，返回该节点的下一个位置
+		iterator erase(iterator pos)
+		{
+			Node *pDelNode = pos._pCur;
+			if (pDelNode == _pHead)
+				return end();
+			Node* pRet = pDelNode->_pNext;//保存返回的下一个结点的位置
+			pDelNode->_pPre->_pNext = pDelNode->_pNext;
+			pDelNode->_pNext->_pPre = pDelNode->_pPre;
+			delete pDelNode;
+			return iterator(pRet);
+		}
+		void clear()
+		{
+			Node* pCur = _pHead->_pNext;
+			//头删法
+			while (pCur != _pHead)
+			{
+				_pHead->_pNext = pCur->_pNext;
+				delete pCur;
+				pCur = pHead->_pNext;
+			}
+			_pHead->_pNext = _pHead;
+			_pHead->_pPre = _pHead;
+		}
+		void swap(list<T>& L)
+		{
+			swap(_pHead, L._pHead);
+		}
+		private:
+			void CreateHead()
+			{
+				_pHead = new Node;
+				_pHead->_pNext = _pHead;
+				_pHead->_pPre = _pHead;
+			}
+		protected:
+			Node* _pHead;
+	};
+}
+int main()
+{
+	return 0;
 }
