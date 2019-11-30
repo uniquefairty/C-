018d391ef510ffa0b7dca7dde92736d11e5c9ab2
uniquefairty <15991160436@163.com>
2019-11-30 23:23:04 +0800

    练习题

--- 考试/考试/main.cpp
+++ 考试/考试/main.cpp
@@ -2700,3 +2700,72 @@ using namespace std;
 	}
 #endif
 
+#if 0
+//歌曲翻页
+#include<string>
+using namespace std;
+int main()
+{
+	int n; 
+	string order;
+	while (cin >> n >> order)
+	{
+		int num = 1;
+		int first = 1;//将n首歌曲编号1：n，
+		//num位光标当前所在歌曲的编号，first为当前屏幕显示页的第一首歌曲的编号
+		if (n <= 4)//歌曲总数不超过4时，所有歌曲一页即可显示完，不需要翻页，first始终不变
+		{
+			for (int i = 0; i < order.size(); i++)
+			{
+				if (num == 1 && order[i] == 'U')
+					num = n;
+				else if (num == n&&order[i] == 'D')
+					num = 1;
+				else if (order[i] == 'U')
+					num--;
+				else
+					num++;
+			}
+			for (int i = 1; i <= n - 1; i++)
+				cout << i << ' ';
+			cout << n << endl;
+			cout << num << endl;
+		}
+		else//歌曲总数大于4时，显示完全所有歌曲需翻页，屏幕总是显示4首歌
+		{
+			for (int i = 0; i < order.size(); i++)
+			{
+				if (num == 1 && order[i] == 'U')//特殊翻页
+				{
+					first -= 3;
+					num = n;
+				}
+				else if (num == n&&order[i] == 'D')//特殊翻页
+				{
+					first = 1;
+					num = 1;
+				}
+				else if (first != 1 && num == first&&order[i] == 'U')//一般翻页
+				{
+					first--;
+					num--;
+				}
+				else if (first != n - 3 && num == first + 3 && order[i] == 'D')//一般翻页
+				{
+					first++;
+					num++;
+				}
+				else if (order[i] == 'U')//其他情况
+					num--;
+				else
+					num++;//其他情况
+			}
+			for (int i = first; i < first + 3; i++)
+				cout << i << ' ';
+			cout << first + 3 << endl;
+			cout << num << endl;
+		}
+	}
+	return 0;
+}
+#endif
--- 考试/考试/test.cpp
+++ 考试/考试/test.cpp
@@ -339,3 +339,18 @@ void perm(int list[], int k, int m)
 	}
 }
 #endif
+
+#if 0
+bool fun(int n){
+	int sum = 0;
+	for (int i = 1; n>sum; i = i + 2)
+		sum = sum + i;
+	return (n == sum);
+}
+int main()
+{
+	cout<<fun(484);
+	return 0;
+}
+#endif
+
