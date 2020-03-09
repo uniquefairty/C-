36a66367556d71ad57ef3002c168c7f7f1f530d3
uniquefairty <15991160436@163.com>
2020-03-09 21:27:55 +0800

    LZ77算法测试

--- HuffmanTreeZip/HuffmanTreeZip/FileCompress.cpp
+++ HuffmanTreeZip/HuffmanTreeZip/FileCompress.cpp
@@ -20,11 +20,16 @@ int main()
 
 #endif 
 
-
+#include <windows.h>
+#include <iostream>
 int main()
 {
 	LZ77 lz;
+	long t1 = GetTickCount();
 	lz.CompressFile("10.txt");
 	lz.UNCompressFile("11.lzp");
+	long t2 = GetTickCount();
+	cout << "运行时间" << (t2 - t1) << endl;
+	
 	return 0;
 }
