4878b9592666bd05916bca78cfb14a90a8e3579e
uniquefairty <15991160436@163.com>
2020-04-04 23:41:13 +0800

    练习题

--- 蓝桥杯/蓝桥杯/main.cpp
+++ 蓝桥杯/蓝桥杯/main.cpp
@@ -486,6 +486,7 @@ printf("fahr=%.1f,celsius=%.1f",fahr, celsius);
 
 #endif
 
+#if 0
 //数组逆序排列
 #include <algorithm>
 #include <iostream>
@@ -535,6 +536,8 @@ int main()
 	return 0;
 }
 
+#endif
+
 #if 0
 #include<stdio.h>
 void swap(int a[], int n)
@@ -563,4 +566,79 @@ int main()
 	}
 	return 0;
 }
+#endif
+
+#if 0
+/*
+问题描述
+　　给定两个仅由大写字母或小写字母组成的字符串(长度介于1到10之间)，它们之间的关系是以下4中情况之一：
+  　　1：两个字符串长度不等。比如 Beijing 和 Hebei
+	　　2：两个字符串不仅长度相等，而且相应位置上的字符完全一致(区分大小写)，比如 Beijing 和 Beijing
+	  　　3：两个字符串长度相等，相应位置上的字符仅在不区分大小写的前提下才能达到完全一致（也就是说，它并不满足情况2）。比如 beijing 和 BEIjing
+		　　4：两个字符串长度相等，但是即使是不区分大小写也不能使这两个字符串一致。比如 Beijing 和 Nanjing
+		  　　编程判断输入的两个字符串之间的关系属于这四类中的哪一类，给出所属的类的编号。
+*/
+#include <iostream>
+using namespace std;
+#include<string>
+
+int main()
+{
+	string s1, s2;
+	cin >> s1 >> s2;
+	int flag = 0;
+	if (s1.size() == s2.size())
+	{
+		for (int i = 0; i < s1.size(); i++)
+		{
+			if (s1[i] != s2[i])//相同位置上的字符不相等
+			{
+				if (s1[i] - s2[i] == 32 || s2[i] - s1[i] == 32)//可以大小写相互替换
+				{
+					flag = 1;
+				}
+				else
+				{
+					cout << "4" << endl;
+					return 0;
+				}
+				
+				
+			}
+		}
+		if (flag == 1)
+		{
+			cout << "3" << endl;
+		}
+		else
+		{
+			cout << "2" << endl;
+		}
+	}
+	else
+	{
+		//字符串长度不相等
+		cout << "1" << endl;
+	}
+	return 0;
+}
+#endif
+
+
+#if 0
+//字符串拼接
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	string s3 = s1 + s2;
+	cout << s3 << endl;
+	return 0;
+}
+
 #endif
