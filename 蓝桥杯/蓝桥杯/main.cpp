3c65931a2bd822fc94acb23ab2dbfb12d28fd4ca
uniquefairty <15991160436@163.com>
2020-04-09 22:01:34 +0800

    练习题

--- 蓝桥杯/蓝桥杯/main.cpp
+++ 蓝桥杯/蓝桥杯/main.cpp
@@ -934,6 +934,7 @@ int main()
 }
 #endif
 
+#if 0
 //2n皇后问题
 #include <iostream>
 #include <cmath>
@@ -1029,5 +1030,57 @@ int main()
 	}
 	Queen_w(0, n);
 	cout << all;
+	return 0;
+}
+
+#endif
+
+//报时助手
+#include <iostream>
+#include <string>
+using namespace std;
+
+string n[100] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+"twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+"thirty", "forty", "fifty" };
+
+void ass(int num)
+{
+	int a = num / 10;
+	int b = num % 10;
+
+	if (num <= 20)
+	{
+		cout << n[num];
+	}
+	else
+	{
+		cout << n[a + 18];
+		if (b != 0)
+		{
+			cout << " " << n[b];
+		}
+	}
+}
+void time_ass(int hour, int minu)
+{
+	if (minu == 0)
+	{
+		ass(hour);
+		cout << "o'clock" << endl;
+	}
+	else
+	{
+		ass(hour);
+		cout << " ";
+		ass(minu);
+		cout << endl;
+	}
+}
+int main()
+{
+	int h, m;
+	cin >> h >> m;
+	time_ass(h, m);
 	return 0;
 }
