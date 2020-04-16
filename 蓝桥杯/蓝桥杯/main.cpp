b3a5f4be75d435a4b05a4397d74ffd5a437d3711
uniquefairty <15991160436@163.com>
2020-04-17 00:00:15 +0800

    练习题

--- 蓝桥杯/蓝桥杯/main.cpp
+++ 蓝桥杯/蓝桥杯/main.cpp
@@ -1192,3 +1192,50 @@ int main()
 	return 0;
 }
 #endif
+
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <algorithm>
+//int main()
+//{
+//	long n, m;
+//	cin >> n >> m;
+//
+//	long res = pow(n, n + 1) - (n - 1)*m;
+//	cout << res;
+//	return 0;
+//}
+
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+using namespace std;
+int main(void)
+{
+	int m, n;
+	cin >> n >> m;
+	for (int i = 0;; i++)
+	{
+		bool flag = true;
+		long long x = i;
+		for (int j = 1; j <= n; j++)
+		{
+			x = x * n + m;
+			if (x % (n - 1) != 0)
+			{
+				flag = false;
+				break;
+			}
+			x = x / (n - 1);
+		}
+		if (flag == false) continue;
+		if (flag == true)
+		{
+			x = x * n + m;
+			printf("%lld", x);
+			return 0;
+		}
+	}
+	return 0;
+}
