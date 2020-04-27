94336c6d0358f649f27c45940ee167ac8fe3e7d6
uniquefairty <15991160436@163.com>
2020-04-27 23:07:44 +0800

    练习题

--- Test/Test/Everyday.cpp
+++ Test/Test/Everyday.cpp
@@ -935,3 +935,93 @@ int main()
 	return 0;
 }
 #endif
+
+#if 0
+#include <iostream>
+using namespace std;
+#include <string>
+#include <algorithm>
+
+bool IsZi(string s1, string s2)
+{
+	if (s1[1] == s2[1])
+	{
+		s2 = s2.substr(0, s1.size());
+		if (s1 == s2)
+			return true;
+	}
+	return false;
+}
+void Mkdir(vector<string> v)
+{
+	string mk = "mkdir -p ";
+	int i = 0;
+	while (i < v.size() - 1)
+	{
+		//前一个字符串是后一个的字串，且后一个字串的下一个位置是/
+		if (IsZi(v[i], v[i + 1]) && v[i + 1][v[i].size()] == '/')
+		{
+			v.erase(v.begin() + i);
+		}
+		else
+			i++;
+	}
+
+	for (auto &e : v)
+		cout << mk << e << endl;
+}
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<string> v;
+		for (int i = 0; i < n; i++)
+		{
+			string s1;
+			cin >> s1;
+			v.push_back(s1);
+		}
+
+		sort(v.begin(), v.end());
+		Mkdir(v);
+		cout << endl;
+	}
+	return 0;
+}
+#endif
+
+#if 0
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int con = 0;
+		queue<char> q;
+		for (int i = 0; i<n; i++)
+		{
+			char ID;
+			cin >> ID;
+			string OP;
+			cin >> OP;
+			if (OP == "connect")
+				q.push(ID);
+			else
+			{
+				con = q.size()>con ? q.size() : con;
+				q.pop();
+			}
+		}
+		cout << con << endl;
+	}
+	return 0;
+}
+#endif
+
