2d89b14c7adbc98643aead5a0cb286087ead6988
uniquefairty <15991160436@163.com>
2020-07-21 23:13:45 +0800

    数组相关算法题

--- Project1/Project1/test.cpp
+++ Project1/Project1/test.cpp
@@ -147,3 +147,94 @@ public:
 	}
 };
 #endif
+
+#if 0
+数组使用，简单算法的设计 （day1）
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组
+{ 1, 2, 3, 2, 2, 2, 5, 4, 2 }。由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+解析：
+//OJ链接：https://www.nowcoder.com/practice/e8a1b01a2df14cb2b228b30ee6a92163?tpId = 13 & tqId = 11181 & rp = 1 & ru = / ta / coding - interviews&qru = / ta / coding - interviews / question - ranking
+//解题思路：
+//这道题整体思路比较明确
+//思路一：定义map，使用<数字，次数>的映射关系，最后统计每个字符出现的次数
+//思路二：排序，出现次数最多的数字，一定在中间位置。然后检测中间出现的数字出现的次数是否符合要求
+//思路三：目标条件：目标数据超过数组长度的一半，那么对数组，我们同时去掉两个不同的数字，到最后剩下的一个数就是
+该数字。如果剩下两个，那么这两个也是一样的，就是结果），在其基础上把最后剩下的一个数字或者两个回到原来数组中，
+将数组遍历一遍统计一下数字出现次数进行最终判断。
+//思路一
+class Solution {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers) {
+		unordered_map<int, int> map;
+		int half = numbers.size() / 2;
+		for (int i = 0; i < numbers.size(); i++){
+			auto it = map.find(numbers[i]);
+			//如果已经在map中，进行自增，如果不在，插入，首次出现
+			if (it != map.end()){
+				map[numbers[i]]++;
+			}
+			else{
+				map.insert(make_pair(numbers[i], 1));
+			}
+			//自增或者插入一个，直接进行判定。注意，这里要考虑测试用例为{1}的情况
+			//走到这里，对应的key val一定存在
+			if (map[numbers[i]] > half){
+				return numbers[i];
+			}
+		}
+		//走到这里，说明没有找到
+		return 0;
+	}
+};
+//思路二
+class Solution {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers) {
+		sort(numbers.begin(), numbers.end());
+		int target = numbers[numbers.size() / 2];
+		int count = 0;
+		for (int i = 0; i < numbers.size(); i++){
+			if (target == numbers[i]){
+				count++;
+			}
+		}
+		if (count > numbers.size() / 2){
+			return target;
+		}
+		return 0;
+	}
+};
+//思路三
+class Solution {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers) {
+		if (numbers.size() == 0){
+			return 0;
+		}
+		//重点写一下
+		//可以采取不同的数量进行抵消的思路
+		int number = numbers[0];
+		int times = 1;
+		for (int i = 1; i < numbers.size(); i++){
+			if (times == 0){ //如果当前times是0，说明之前的不同抵消完了
+				number = numbers[i];
+				times = 1;
+			}
+			else if (numbers[i] == number){
+				times++;
+			}
+			else{
+				times--;
+			}
+		}
+		//如果输入本身满足条件，则times一定>0, 并且number保存的就是准目标，但是还需要确认
+		int count = 0;
+		for (int i = 0; i < numbers.size(); i++){
+			if (numbers[i] == number){
+				count++;
+			}
+		}
+		return count > numbers.size() / 2 ? number : 0;
+	}
+};
+#endif
