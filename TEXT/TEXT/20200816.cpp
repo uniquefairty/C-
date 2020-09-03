61ffe07b901b4a28f0be973020e779e188c2314b
uniquefairty <15991160436@163.com>
2020-09-03 23:00:40 +0800

    能看见的高楼层数

--- TEXT/TEXT/20200816.cpp
+++ TEXT/TEXT/20200816.cpp
@@ -29,3 +29,65 @@ int main()
 	return 0;
 }
 #endif
+
+//看高楼层数--错误
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> v;
+	vector<int> result;
+	v.resize(n);
+	result.resize(n, 1);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		//判断前部分
+		if (i != 0)
+		{
+			//result[i]++;
+			int j = i;
+			//int pre = v[j - 1];//保存前面的值
+			//int maxpre = pre;//前面的最大值
+			int maxpre = v[j];
+			while (--j)
+			{		
+				if (v[j - 1] > maxpre)
+				{
+					result[i]++;
+					maxpre = v[j - 1];
+				}
+			}
+
+		}
+		//判断后部分
+		if (i != v.size())
+		{
+			result[i]++;
+			int j = i;
+			int maxafter = v[j];
+			while (++j&&j < v.size()-1)
+			{
+				if (v[j + 1]>maxafter)
+				{
+					result[i]++;
+					maxafter = v[j + 1];
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < result.size(); i++)
+	{
+		cout << result[i] << ' ';
+	}
+	return 0;
+}
