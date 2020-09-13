5757105f6190f63199e3e02a412b0682610edffb
uniquefairty <15991160436@163.com>
2020-09-13 16:24:23 +0800

    笔试题

--- TEXT/TEXT/dfs.cpp
+++ TEXT/TEXT/dfs.cpp
@@ -142,7 +142,7 @@ int main()
 }
 #endif
 
-#if 1
+#if 0
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -250,3 +250,185 @@ int main()
 }
 #endif
 
+#if 0
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool fun(vector<int> a, vector<int> b)
+{
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (a[i] != b[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+int same(vector<vector<int>> a,int index,int& res)
+{
+	res = index;
+	for (int i = 0,j=index/2; i < index / 2; i++,j++)
+	{
+		if (!fun(a[i], a[j]))
+		{
+			return res;
+		}
+	}
+	same(a, index / 2,res);
+	return res;
+	
+	
+}
+int main()
+{
+	int m, n;
+	while (cin >> m >> n)
+	{
+		int pos = 0;
+		vector<vector<int>> v;
+		v.resize(m);
+		for (int i = 0; i < m; i++)
+		{
+			v[i].resize(n);
+			for (int j = 0; j < n; j++)
+			{
+				cin >> v[i][j];
+			}		
+		}
+		pos=same(v, v.size(),pos);
+		pos = pos / 2;
+		for (int i = 0; i < pos; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cout << v[i][j] << ' ';
+			}
+			cout << endl;
+		}
+	
+
+	}
+	return 0;
+}
+#endif
+
+#if 0
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+bool fun(vector<int> v, int m, int k, int index)
+{
+	while (m)
+	{
+		if (v[index] < k)
+		{
+			return false;
+			break;
+		}
+		index++;
+		m--;
+	}
+	return true;
+}
+int main()
+{
+	int n, m, k;
+	while (cin >> n >> m >> k)
+	{
+		vector<int> v;
+		v.resize(n);
+		int res = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> v[i];
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			int count = m;
+			int j = i;
+			int flag = 1;
+			while (j < n && count)
+			{
+				if (v[j] < k)
+				{
+					flag = 0;
+					break;
+					
+				}
+				j++;
+				count--;
+				
+			}
+			if (!count&&flag == 1)
+			{
+				res += 1;
+			}
+		}
+		cout << res << endl;
+	}
+	return 0;
+}
+#endif
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int find_max(vector<int> v, vector<bool>& visit)
+{
+	int pos = 0;
+	int max = v[0];
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (v[i] > max && visit[i])
+		{
+			max = v[i];
+			pos = i;
+		}
+	}
+	visit[pos] = false;
+	return pos;
+}
+int find_second(vector<int>v, vector<bool> visit)
+{
+	find_max( v, visit);
+	return find_max(v, visit);
+}
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<vector<int>> v;
+		v.resize(2);
+		for (int i = 0; i < 2; i++)
+		{
+			v[i].resize(n);
+			for (int j = 0; j < n; j++)
+			{
+				cin >> v[i][j];
+			}
+		}
+
+		vector<bool> visit(n, true);
+		//≈≈–Ú
+		while (n)
+		{
+			int index = find_max(v[1], visit);
+			int pos = find_second(v[1], visit);
+			cout << index + 1 << ' ';
+			n--;
+
+		}
+	}
+	return 0;
+}
